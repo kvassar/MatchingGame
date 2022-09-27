b8047736323dac751602db41e3b8ce4cb036fef1
Kal Vassar <kvassar@hbs.net>
2022-09-27 12:55:46 -0500

    Add project files.

---
+++ MatchingGame/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "%", "%", "*", "*", "^", "^"
+            "$", "$", "@", "@", "#", "#", "(", "(" 
+        };
+        public Form1()
+
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
